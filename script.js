bebf4c25e6cdab89fe078da679a9ad4c3bdba3f7
boyblacks101-tech <boyblacks101@gmail.com>
2026-08-13 00:16:12 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,23 @@
-let xp = Number(localStorage.getItem("xp")) || 0;
-let level = Number(localStorage.getItem("level")) || 1;
-let completedQuests =
-    Number(localStorage.getItem("completedQuests")) || 0;
+const xp = 0;
+const level = 1;
 
-const xpText = document.getElementById("xpText");
-const xpBar = document.getElementById("xpBar");
-const levelElement = document.getElementById("level");
-const questCount = document.getElementById("questCount");
+const xpElement =
+    document.getElementById("xp");
 
-function saveGame() {
-    localStorage.setItem("xp", xp);
-    localStorage.setItem("level", level);
-    localStorage.setItem(
-        "completedQuests",
-        completedQuests
-    );
-}
+const levelElement =
+    document.getElementById("level");
 
-function updateUI() {
-    xpText.textContent = `${xp} / 100`;
-    xpBar.style.width = `${xp}%`;
+const xpBar =
+    document.getElementById("xpBar");
+
+function updateCharacter() {
+
+    xpElement.textContent = xp;
+
     levelElement.textContent = level;
 
-    questCount.textContent =
-        `${completedQuests} / 3`;
+    xpBar.style.width = `${xp}%`;
+
 }
 
-function completeQuest(button, reward) {
-    if (button.classList.contains("completed")) {
-        return;
-    }
-
-    button.classList.add("completed");
-
-    button.querySelector(
-        ".quest-check"
-    ).textContent = "✓";
-
-    completedQuests++;
-    xp += reward;
-
-    if (xp >= 100) {
-        xp -= 100;
-        level++;
-    }
-
-    saveGame();
-    updateUI();
-}
-
-updateUI();
+updateCharacter();
